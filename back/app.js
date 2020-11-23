d68c25f1a87c754558df56622d590c5041eb3ead
nathaneast <ahrl_ahrl@naver.com>
2020-11-23 14:04:28 +0900

    modify server app, package.json

--- back/app.js
+++ back/app.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const path = require('path');
+const hpp = require('hpp');
+const helmet = requrie('helmet');
 
 const postRouter = require('./routes/post');
 const postsRouter = require('./routes/posts');
@@ -24,9 +26,15 @@ db.sequelize.sync()
 
 passportConfig();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan('dev'));
+}
 app.use(cors({
-  origin: true,
+  origin: ['http://localhost:3060', 'nodebird.com'],
   credentials: true,
 }));
 app.use('/', express.static(path.join(__dirname, 'uploads')));
