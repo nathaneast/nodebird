2e74850d017cbb42755ce804291bf33bdc8c1e94
nathaneast <ahrl_ahrl@naver.com>
2020-11-23 14:34:12 +0900

    back: update origin

--- back/app.js
+++ back/app.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
 }
 app.use(cors({
-  origin: ['http://localhost:3060', 'nodebird.com'],
+  origin: ['http://localhost:3060'],
   credentials: true,
 }));
 app.use('/', express.static(path.join(__dirname, 'uploads')));
